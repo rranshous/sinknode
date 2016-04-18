11099047cb7682f9b526740c42037036e776a702
Robby Ranshous <rranshous@gmail.com>
2016-04-17 21:06:22 -0400

    closer

--- sinknode.rb
+++ sinknode.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'io'
-
 # data flows down
 # there are 8 levels
 # each node has a level
@@ -11,22 +9,28 @@ require 'io'
 
 # level should be defined and be an integer
 level = ARGV.shift
-raise 'missing level' if level.empty?
+raise 'missing level' if level.nil?
 level = level.to_i
+source_host = ARGV.shift
+raise "missing suorce host" if source_host.nil?
+target_host = ARGV.shift
+raise "missing target_host" if target_host.nil?
 
 # open up a subprocesses which subscribes to all level messages
-sub_command = "mosquitto_sub -v -h #{source_host} -t level/# -q 2"
+sub_command = "docker run -it rranshous/mosquitto mosquitto_sub -v -h #{source_host} -t 'level/#' -q 2 -i bridge-#{level}-#{source_host}-#{target_host}"
 puts "opening subprocesses with command: #{sub_command}"
 sub_io = IO.popen(sub_command)
 puts "sub_io: #{sub_io}"
 
 # go through the messages as we receive them
-sub_io.readlines.each do |line|
+puts "reading lines"
+while line = sub_io.readline do
   puts "line: #{line}"
-  topic, data = data.split "\n", 2
+  topic, data = line.split " ", 2
   puts "topic: #{topic}"
   puts "data: #{data}"
   _, level = topic.split('/',3)
+  level = level.to_i
   puts "level: #{level}"
   # we only accept data from nodes "higher" than us
   if level.to_i <= level
@@ -35,7 +39,7 @@ sub_io.readlines.each do |line|
   end
   # use the pub command to push this message down hill
   # maintain the topic
-  pub_command = "mosquitto_pub -h #{target_host} -t #{topic} -s"
+  pub_command = "docker run -it rranshous/mosquitto mosquitto_pub -h #{target_host} -t #{topic} -s"
   puts "opening subprocesses with command: #{pub_command}"
   pub_io = IO.popen(pub_command)
   puts "pub_io: #{pub_io}"
@@ -44,4 +48,9 @@ sub_io.readlines.each do |line|
   puts "done writing"
 end
 
+puts "detaching"
+Process.detach(sub_io.pid)
+
 puts "done reading lines"
+
+at_exit { puts "exiting"; Process.kill(sub_io) }
