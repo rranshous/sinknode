9eee0b5e0aceefeb5533909e7ce143298be91b27
Robby Ranshous <rranshous@gmail.com>
2016-05-15 15:34:00 -0400

    accepts ports, trying to get output

--- sinknode.rb
+++ sinknode.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'pty'
+
 # data flows down
 # there are 8 levels
 # each node has a level
@@ -12,49 +14,57 @@ our_level = ARGV.shift
 raise 'missing level' if our_level.nil?
 our_level = our_level.to_i
 source_host = ARGV.shift
+source_port = source_host.split(':',2).last || '1883'
+source_host = source_host.split(':',2).first
 raise "missing suorce host" if source_host.nil?
 target_host = ARGV.shift
+target_port = target_host.split(':',2).last || '1883'
+target_host = target_host.split(':',2).first
 raise "missing target_host" if target_host.nil?
 
 # open up a subprocesses which subscribes to all level messages
-sub_command = "docker run -i rranshous/mosquitto mosquitto_sub -v -h #{source_host} -t 'level/#' -q 2 -i bridge-#{our_level}-#{source_host}-#{target_host}"
+sub_command = "docker run -it rranshous/mosquitto mosquitto_sub -v -h #{source_host} -p #{source_port} -t 'level/#' -q 2 -i bridge-#{our_level}-#{source_host}-#{target_host}"
 puts "opening subprocesses with command: #{sub_command}"
-sub_io = IO.popen(sub_command)
-puts "sub_io: #{sub_io}"
 
-# go through the messages as we receive them
-puts "reading lines"
-while line = sub_io.readline do
-  if line.empty?
-    puts "blank line?"
-    next
+PTY.spawn sub_command do |r, w, pid|
+  begin
+    puts "syncing"
+    r.sync
+    puts "reading"
+    r.each_line do |line|
+      puts "line: #{line}"
+      if line.empty?
+        puts "blank line?"
+        next
+      end
+      puts "line: #{line}"
+      topic, data = line.split " ", 2
+      puts "topic: #{topic}"
+      puts "data: #{data}"
+      _, level = topic.split('/',3)
+      level = level.to_i
+      puts "level: #{level}"
+      # we only accept data from nodes "higher" than us
+      if level <= our_level
+        puts "skipping message, too low level"
+        next
+      end
+      # use the pub command to push this message down hill
+      # maintain the topic
+      pub_command = "docker run -i rranshous/mosquitto mosquitto_pub -h #{target_host} -p #{target_port} -t #{topic} -s"
+      puts "opening subprocesses with command: #{pub_command}"
+      pub_io = IO.popen(pub_command)
+      puts "pub_io: #{pub_io}"
+      puts "writing to pub_io: #{data}"
+      pub_io.write(data)
+      puts "done writing"
+    end
+  rescue Errno::EIO => e
+    puts "EIO error: #{e}"
+  ensure
+    ::Process.wait pid
   end
-  puts "line: #{line}"
-  topic, data = line.split " ", 2
-  puts "topic: #{topic}"
-  puts "data: #{data}"
-  _, level = topic.split('/',3)
-  level = level.to_i
-  puts "level: #{level}"
-  # we only accept data from nodes "higher" than us
-  if level <= our_level
-    puts "skipping message, too low level"
-    next
-  end
-  # use the pub command to push this message down hill
-  # maintain the topic
-  pub_command = "docker run -i rranshous/mosquitto mosquitto_pub -h #{target_host} -t #{topic} -s"
-  puts "opening subprocesses with command: #{pub_command}"
-  pub_io = IO.popen(pub_command)
-  puts "pub_io: #{pub_io}"
-  puts "writing to pub_io: #{data}"
-  pub_io.write(data)
-  puts "done writing"
 end
-
-puts "detaching"
-Process.detach(sub_io.pid)
 
 puts "done reading lines"
 
-at_exit { puts "exiting"; Process.kill(sub_io) }
