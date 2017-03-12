b885a231e405f8313cbaaf8f82475fd2ec64b55c
Robby Ranshous <rranshous@gmail.com>
2017-03-12 17:07:37 -0400

    use subfolders for drained files

--- bin/drain.rb
+++ bin/drain.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'mqtt'
+require 'fileutils'
+
 def log msg
   STDOUT.write("#{msg}\n")
   STDOUT.flush
@@ -8,16 +12,19 @@ end
 log "STARTING"
 
 def write_file name, data
+  is_retry = false
   name = "NONAME" if name.nil? || name.empty?
   base_out = File.absolute_path './data'
-  name_with_timestamp = "#{name}-#{Time.now.to_f}"
-  out_path = File.join base_out, name_with_timestamp
+  timestamp = Time.now.to_f.to_s
+  out_path = File.join base_out, name, timestamp
   log "writing: #{name} :: #{data.length} => #{out_path}"
   File.write(out_path, data)
+rescue Errno::ENOENT
+  raise if is_retry
+  FileUtils.mkdir_p File.dirname(out_path)
+  is_retry = true
+  retry
 end
-
-require 'rubygems'
-require 'mqtt'
 
 server_host = ARGV.shift
 
